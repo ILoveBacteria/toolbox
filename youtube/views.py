5081086f1857d1e7367b29c8929b4ba0997d621d
Moein Arabi <moein.mo81@gmail.com>
2023-08-25 19:25:10 +0330

    Add download video

--- youtube/views.py
+++ youtube/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .forms import DownloadVideoForm
 
-# Create your views here.
+
+def download_video(request):
+    if request.method == "POST":
+        form = DownloadVideoForm(request.POST)
+        if form.is_valid():
+            return HttpResponse(form.cleaned_data['link'])
+        return render(request, 'youtube/download_video.html', context={'form': form}, status=400)
+    else:
+        form = DownloadVideoForm()
+        return render(request, 'youtube/download_video.html', context={'form': form})
