f3713be95269463ffc27ec2a39cbbc50321d2b4a
Moein Arabi <moein.mo81@gmail.com>
2023-09-05 20:16:54 +0330

    Add models to admin panel

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import VideoModel, PlaylistModel
 
-# Register your models here.
+
+@admin.register(VideoModel)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'playlist')
+
+
+@admin.register(PlaylistModel)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('title', 'count_videos')
+
+    def count_videos(self, obj: PlaylistModel):
+        return obj.videomodel_set.count()
