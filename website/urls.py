fc932c38dbc51cc09903cee512fffbbf3e061cb9
Moein Arabi <moein.mo81@gmail.com>
2023-09-06 11:41:54 +0330

    Add api views

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('api/education'),
-    path('api/experience'),
-    path('api/certificate'),
+    path('api/education', views.EducationList.as_view()),
+    path('api/experience', views.ExperienceList.as_view()),
+    path('api/certificate', views.CertificateList.as_view()),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+class EducationList(generics.ListAPIView):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+
+
+class ExperienceList(generics.ListAPIView):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+
+class CertificateList(generics.ListAPIView):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
