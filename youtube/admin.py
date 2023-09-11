be0f3905eb1956de1522ecf1881f9cc0a9f5889e
Moein Arabi <moein.mo81@gmail.com>
2023-09-11 21:21:03 +0330

    Delete video with actions

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,10 +1,17 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Video, Playlist
 
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'playlist')
+    actions = ['delete_with_video_file']
+
+    @admin.action
+    def delete_with_video_file(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, 'Successfully delete with videos', messages.SUCCESS)
 
 
 @admin.register(Playlist)
