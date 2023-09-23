3715f7708f4b6b33b36af0a9a6d01fff71a28055
Moein Arabi <moein.mo81@gmail.com>
2023-09-23 23:35:56 +0330

    Remove unused form class

--- accounts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label='username')
-    password = forms.CharField(label='password', widget=forms.PasswordInput)
