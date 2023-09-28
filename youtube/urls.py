1bafbd8c1c0f6381cbd6a6e02f597e6e191839d6
Moein Arabi <moein.mo81@gmail.com>
2023-09-28 12:54:41 +0330

    Clean code

--- youtube/urls.py
+++ youtube/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import DownloadPlaylistView, DownloadVideoView, PlaylistDetailView, PlaylistListView, VideoDetailView, \
-    VideoListView, VideoUpdateView, PlaylistUpdateView, VideoDeleteView
+from . import views
+
 
 urlpatterns = [
-    path('playlist/', DownloadPlaylistView.as_view(), name='playlist'),
-    path('playlists/update/<int:pk>', PlaylistUpdateView.as_view(), name='playlist_update'),
-    path('playlists/<int:pk>', PlaylistDetailView.as_view(), name='playlist_detail'),
-    path('playlists/', PlaylistListView.as_view(), name='playlist_list'),
-    path('video/', DownloadVideoView.as_view(), name='video'),
-    path('videos/', VideoListView.as_view(), name='video_list'),
-    path('videos/delete/<int:pk>', VideoDeleteView.as_view(), name='video_delete'),
-    path('videos/update/<int:pk>', VideoUpdateView.as_view(), name='video_update'),
-    path('videos/<int:pk>', VideoDetailView.as_view(), name='video_detail'),
+    path('playlist/', views.DownloadPlaylistView.as_view(), name='playlist'),
+    path('playlists/update/<int:pk>', views.PlaylistUpdateView.as_view(), name='playlist_update'),
+    path('playlists/<int:pk>', views.PlaylistDetailView.as_view(), name='playlist_detail'),
+    path('playlists/', views.PlaylistListView.as_view(), name='playlist_list'),
+    path('video/', views.DownloadVideoView.as_view(), name='video'),
+    path('videos/', views.VideoListView.as_view(), name='video_list'),
+    path('videos/delete/<int:pk>', views.VideoDeleteView.as_view(), name='video_delete'),
+    path('videos/update/<int:pk>', views.VideoUpdateView.as_view(), name='video_update'),
+    path('videos/<int:pk>', views.VideoDetailView.as_view(), name='video_detail'),
 ]
