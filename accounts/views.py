7bb5dc44cc4c3ce5e0d1c729f36328f6eee74b3e
Moein Arabi <moein.mo81@gmail.com>
2024-02-19 20:55:18 +0330

    Prevent open redirect attack

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import login, logout as dj_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
+from django.utils.http import url_has_allowed_host_and_scheme
 from django.views.generic import FormView
+
+from toolbox.settings.base import ALLOWED_HOSTS
 
 
 class Login(FormView):
@@ -13,7 +17,9 @@ class Login(FormView):
     def form_valid(self, form):
         login(self.request, form.get_user())
         to = self.request.GET.get('next') or self.success_url
-        return redirect(to)
+        if url_has_allowed_host_and_scheme(url=to, allowed_hosts=ALLOWED_HOSTS):
+            return redirect(to)
+        return HttpResponseBadRequest()
 
 
 def logout(request):
