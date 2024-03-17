ff36961315935bcfc4d6bd39771f24d78ec56ea5
Moein Arabi <moein.mo81@gmail.com>
2024-03-17 22:09:23 +0330

    Add gunicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py collectstatics --no-input
+python manage.py collectstatic --no-input
 python -m celery -A toolbox worker -l info &
 python -m celery -A toolbox beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 toolbox.wsgi:application
